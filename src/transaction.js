7a491732c34cc1091e16cfc8a065b639568d6566
Hector Huang <h94huang@gmail.com>
2019-11-10 10:03:15 -0500

    for demo

--- src/transaction.js
+++ src/transaction.js
@@ -17,9 +17,9 @@ function Transaction(bitcoins, highlight, currency, currencyName) {
 	    if(globalShowDollar === true) {
     		bitcoinString = "$" + (bitcoins*globalRate).toFixed(0);
     	} else if (bitcoinVal === "0.00") {
-	        bitcoinString = "&lt;<span class='bitcoinsymbol'>B</span>0.01";
+	        bitcoinString = "&lt;<td>Ξ</td>0.01";
 	    } else {
-	        bitcoinString = "<span class='bitcoinsymbol'>B</span>" + bitcoinVal;
+	        bitcoinString = "<td>Ξ</td>" + bitcoinVal;
 	    }
 	
 		if (!highlight) {
